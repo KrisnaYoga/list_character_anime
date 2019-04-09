4208afd7e4574911ae8271485d41b2142d5e32ee
KrisnaYoga <adityakrisna456@gmail.com>
2019-04-09 22:05:23 +0800

    Yuusuke Fujisaki -> Done

--- app/src/main/java/com/a1705551024/list_character_anime/MainActivity.java
+++ app/src/main/java/com/a1705551024/list_character_anime/MainActivity.java
@@ -1,13 +1,31 @@
 package com.a1705551024.list_character_anime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnfujisaki;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnfujisaki = findViewById(R.id.btn_fuujisaki);
+        btnfujisaki.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_fuujisaki:
+            Intent goes_to_yuusuke_fujisaki_activity = new Intent(MainActivity.this, Yusuke_Fujisaki_Det.class);
+            startActivity(goes_to_yuusuke_fujisaki_activity);
+            break;
+        }
     }
 }
---
+++ app/src/main/java/com/a1705551024/list_character_anime/Yusuke_Fujisaki_Det.java
@@ -0,0 +1,17 @@
+package com.a1705551024.list_character_anime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Yusuke_Fujisaki_Det extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yusuke__fujisaki__det);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Yusuke Fujisaki");
+        }
+    }
+}
