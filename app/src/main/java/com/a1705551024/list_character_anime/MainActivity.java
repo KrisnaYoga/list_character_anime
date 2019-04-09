8a45ce288157917475ebae1e7378f7dc48649aab
KrisnaYoga <adityakrisna456@gmail.com>
2019-04-09 20:34:36 +0800

    Base

---
+++ app/src/main/java/com/a1705551024/list_character_anime/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a1705551024.list_character_anime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
